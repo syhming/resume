340e3a3c6d943564a6ad9d5195c6a1f555eabbae
syhming <syhming@gmail.com>
2016-08-01 10:47:18 -0400

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,14 +6,8 @@ var bio = {
     "welcomeMessage": "Welcome to my resume page!  I'm an artist learning new skills in a different arena.",
     "contacts": {
         "email": "syhming@gmail.com",
-        "github": {
-            'name': "syhming",
-            'url': 'https://github.com/syhming'
-        },
-        "twitter": {
-            'name': "@syhming",
-            'url': 'https://twitter.com/syhming'
-        },
+        "github": "syhming",
+        "twitter": "@syhming",
         "location": "Holland, MI"
     },
     "skills": ["HTML", "CSS", "javascript", "illustration", "painting", "photography", "drawing"],
@@ -27,9 +21,9 @@ bio.display = function() {
     //TODO make github and twitter links aligned with the rest of contacts
     var headerEmail = HTMLemail.replace('%data%', bio.contacts.email);
     $('#topContacts').append(headerEmail);
-    var headerGithub = HTMLgithub.replace('%data%', bio.contacts.github.name).replace('#', bio.contacts.github.url);
+    var headerGithub = HTMLgithub.replace('%data%', bio.contacts.github);
     $('#topContacts').append(headerGithub);
-    var headerTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter.name).replace('#', bio.contacts.twitter.url);
+    var headerTwitter = HTMLtwitter.replace('%data%', bio.contacts.twitter);
     $('#topContacts').append(headerTwitter);
     var headerLocation = HTMLlocation.replace('%data%', bio.contacts.location);
     $('#topContacts').append(headerLocation);
@@ -45,20 +39,9 @@ bio.display = function() {
         }
     }
 
-
     var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
-    $('#header').prepend(formattedBioPic);
-    $('#header').prepend(headerRole);
-    $('#header').prepend(headerName);
-    $('#footerContacts').append(headerEmail);
-    $('#footerContacts').append(headerTwitter);
-    $('#footerContacts').append(headerGithub);
-
-    $("a[target='_blank']").css({
-        'color': '#fff',
-        'margin-top': '-10px',
-        'padding': '5px 0 0 0'
-    });
+    $('#header').prepend(formattedBioPic, headerName, headerRole);
+    $('#footerContacts').append(headerEmail, headerTwitter, headerGithub);
 };
 bio.display();
 
@@ -78,25 +61,25 @@ var work = {
             "employer": "Self-employed",
             "title": "Artist",
             "location": "Holland, MI",
-            "date": "June 2016 - present",
+            "dates": "June 2016 - present",
             "description": "Creating original oil paintings depicting people in reaction to their hopes and dreams."
         }, {
             "employer": "Gentex",
             "title": "Production Team Member",
             "location": "Zeeland, MI",
-            "date": "July 2015 - present",
+            "dates": "July 2015 - present",
             "description": "Producing and inspecting inside rearview mirrors for automobiles."
         }, {
             "employer": "Manpower",
             "title": "Temporary Production Assistant",
             "location": "Zeeland, MI",
-            "date": "May 2007 - June 2015",
+            "dates": "May 2007 - June 2015",
             "description": "Working in various factories learning how to assemble parts, work with fiberglass and epoxy, and inspect for defects."
         }, {
             "employer": "Corporate Color Graphics",
             "title": "Photo Mounter",
             "location": "Grand Rapids, MI",
-            "date": "June 2014 - September 2014",
+            "dates": "June 2014 - September 2014",
             "description": "Laminating photos and mounting them onto boards that gets edge trimmed."
         }]
         /*this function shows all the info written in work*/
@@ -109,7 +92,7 @@ work.display = function() {
         var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
         var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
 
-        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].date);
+        var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
         var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
         var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
 
@@ -124,26 +107,26 @@ work.display();
 /*info on schools, certifications and online courses taken*/
 var education = {
     "schools": [{
-        "name": "Michigan State University",
+        "school": "Michigan State University",
         "location": "East Lansing, MI, US",
         "degree": "BFA",
-        "major": "Studio Art",
-        "date": "2007 - 2008"
+        "majors": "Studio Art",
+        "dates": "2007 - 2008"
     }],
     "onlineCourses": [{
-        "site": "Udacity",
+        "school": "Udacity",
         "title": "Front-end Web Development",
-        "date": "August 2015 - present",
+        "dates": "August 2015 - present",
         "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
     }, {
-        "site": "Udacity",
+        "school": "Udacity",
         "title": "Intro to HTML and CSS",
-        "date": "August 2015 - October 2015",
+        "dates": "August 2015 - October 2015",
         "url": 'https://www.udacity.com/course/progress#!/c-ud304-nd'
     }, {
-        "site": "Udacity",
+        "school": "Udacity",
         "title": "Javascript Basics",
-        "date": "October 2016 - February 2016",
+        "dates": "October 2016 - February 2016",
         "url": "https://www.udacity.com/course/progress#!/c-ud804-nd"
     }]
 };
@@ -152,29 +135,28 @@ education.display = function() {
     if (education.schools.length) {
         for (var school = 0; school < education.schools.length; school++) {
             $('#education').append(HTMLschoolStart);
-            var schoolName = HTMLschoolName.replace('%data%', education.schools[school].name);
+            var schoolName = HTMLschoolName.replace('%data%', education.schools[school].school);
             var schoolDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
             var schoolNameDegree = schoolName + schoolDegree;
             $('.education-entry:last').append(schoolNameDegree);
 
             var schoolLocation = HTMLschoolLocation.replace('%data%', education.schools[school].location);
             $('.education-entry:last').append(schoolLocation);
-            var schoolDate = HTMLschoolDates.replace('%data%', education.schools[school].date);
-            $('.education-entry:last').append(schoolDate);
-            var schoolMajor = HTMLschoolMajor.replace('%data%', education.schools[school].major);
+            var schoolDates = HTMLschoolDates.replace('%data%', education.schools[school].dates);
+            $('.education-entry:last').append(schoolDates);
+            var schoolMajor = HTMLschoolMajor.replace('%data%', education.schools[school].majors);
             $('.education-entry:last').append(schoolMajor);
 
         }
         // TODO: "Online Courses" header aligned the same as classes
 
-        $('.education-entry:last').append(HTMLonlineClasses);
-        $(":contains('Courses')").removeAttr("padding");
+        $('#education').append(HTMLonlineClasses);
         for (var course = 0; course < education.onlineCourses.length; course++) {
             var formattedOnlineTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title);
-            var formattedOnlineSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[course].site);
+            var formattedOnlineSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school);
             var formattedTitleSchool = formattedOnlineTitle + formattedOnlineSchool;
             $('.education-entry:last').append(formattedTitleSchool);
-            var formattedOnlineDates = HTMLonlineDates.replace('%data%', education.onlineCourses[course].date);
+            var formattedOnlineDates = HTMLonlineDates.replace('%data%', education.onlineCourses[course].dates);
             $('.education-entry:last').append(formattedOnlineDates);
         }
     }
@@ -185,7 +167,7 @@ education.display();
 var projects = {
     "project": [{
         "title": "Portfolio Site",
-        "date": "August 2015 - September 2015",
+        "dates": "August 2015 - September 2015",
         "description": "First project for the Udacity Nanodegree.  I learned to create a portfolio site using HTML and CSS.",
         "image": [
             "http://placehold.it/350x150",
@@ -194,7 +176,7 @@ var projects = {
         ]
     }, {
         "title": "Resume",
-        "date": "In progress",
+        "dates": "In progress",
         "description": "Second project for the nanodegree.  I built a resume using javascript.",
         "image": [
             "http://placehold.it/350x150",
@@ -210,8 +192,8 @@ projects.display = function() {
 
         var projectTitle = HTMLprojectTitle.replace('%data%', projects.project[item].title);
         $('.project-entry:last').append(projectTitle);
-        var projectDate = HTMLprojectDates.replace('%data%', projects.project[item].date);
-        $('.project-entry:last').append(projectDate);
+        var projectDates = HTMLprojectDates.replace('%data%', projects.project[item].dates);
+        $('.project-entry:last').append(projectDates);
         var projectDescription = HTMLprojectDescription.replace('%data%', projects.project[item].description);
         $('.project-entry:last').append(projectDescription);
 
